9a4cdba2cf9b97d44a4aba5c4025f39ed6c8dece
fumi uk <dcfumiya.238@gmail.com>
2020-09-17 08:32:09 +0900

    quick-react

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MyBook from './MyBook';
+import MyHello from './MyHello';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const book = {
+  isbn:'WGS-JST-001',
+  title:'速習 webpack',
+  price:454,
+  published:'WINGSプロジェクト'
+};
+
+ReactDOM.render(
+  <MyHello />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/index_pra.js
@@ -0,0 +1,6 @@
+const message='こんにちは、世界！';
+
+ReactDom.render(
+<h1>{message}</h1>,
+document.getElementById('roots')
+);
